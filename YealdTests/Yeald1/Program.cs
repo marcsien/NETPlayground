8b5f4f20e7a4f4415fbd98c8ed6e09d156f52a7e
marcsien <marcsien@gmail.com>
2022-03-12 11:57:44 +0100

    Add new solution with two test projects with yeald return

---
+++ YealdTests/Yeald1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yeald1
+{
+    class Program
+    {
+        static List<int> MyList = new List<int>();
+        static void Main(string[] args)
+        {
+            // wypełniamy naszą listę danymi
+            FillMyList();
+            // wywołujemy pętle foreach na naszej metodzie, która zwraca jedynie liczby większe niż 3
+            foreach (var item in ListWithYieldReturn())
+            {
+                // wyświetlamy wartości, które spełniają warunek przez nas zdefiniowany
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+        static void FillMyList()
+        {
+            MyList.Add(1);
+            MyList.Add(2);
+            MyList.Add(3);
+            MyList.Add(4);
+            MyList.Add(5);
+        }
+        static IEnumerable<int> ListWithYieldReturn()
+        {
+            // Przechodzimy po elementach naszej listy
+            foreach (var item in MyList)
+            {
+                // kontrolka do caller'a (metody wywołującej) jest przekazywana jeżeli warunek jest spełniony
+                if (item > 3)
+                    yield return item;
+            }
+        }
+    }
+}
---
+++ YealdTests/Yeald2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yeald2
+{
+    class Program
+    {
+        static List<int> MyList = new List<int>();
+        static void Main(string[] args)
+        {
+            FillMyList();
+            foreach (var item in RunningTotal())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        static IEnumerable<int> RunningTotal()
+        {
+            int runningTotal = 0;
+            foreach (var item in MyList)
+            {
+                runningTotal += item;
+                yield return runningTotal;
+            }
+        }
+        static void FillMyList()
+        {
+            MyList.Add(1);
+            MyList.Add(2);
+            MyList.Add(3);
+            MyList.Add(4);
+            MyList.Add(5);
+        }
+    }
+}
