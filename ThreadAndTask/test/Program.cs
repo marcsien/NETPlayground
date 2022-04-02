4c1c775049cf8a5d3093d1f72433868d967322c0
marcsien <marcsien@gmail.com>
2022-04-02 14:15:13 +0200

    Add Thread and Task comparer (not my code)

---
+++ ThreadAndTask/test/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ParallelTasksExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxThreadAllowed = 64;
+            var watch = new Stopwatch();
+            // Klasa ManualResetEventSlim to lżejsza wersja klasy ManualResetEvent
+            // Lżejszej wersji używamy dla krótkich operacji, gdzie blokada nie będzie trwała długo
+            // ManualResetEvent używamy, gdy operacja będzie trwała długo a metoda WaitOne będzie
+            // blokowała wątek przez długi czas
+            // Klasa jest implemetacją po stronie jądra, a takie operacje, tj. .NET-kernel są zwykle bardzo dużym ociążaniem dla procesora i pamięci.
+            // Jednym z przykładów użycia klasy jest oczekiwanie na zamknięcie przez pewien określony czas oraz przerwanie, jeżeli nie istnieje.
+            // Przypadek taki może się zdarzyć, kiedy wątek utknie np. w tzw. deadlock'u - martwy punkt.
+            var mres = new ManualResetEventSlim[maxThreadAllowed];
+            for (int i = 0; i < mres.Length; i++)
+            {
+                // poniższy zapis pozwala na ustawienie stanu początkowego obiektu, w tym przypadku ustawiamy go na false dla każdego elementu
+                // innymi słowy, początkowa wartość sygnału jest ustawiona na false
+                mres[i] = new ManualResetEventSlim(false);
+            }
+
+
+
+
+            watch.Start();
+            // uruchamiamy nowy klasyczny wątek (thread) oraz powiadamiamy ManualResetEvent kiedy wykonanie jest skończone
+            // możemy więc sprawdzić czas wykonania
+            for (int i = 0; i < mres.Length; i++)
+            {
+                var id = i;
+                var thread = new Thread(state =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.WriteLine(string.Format("Wątek: {0}, numer operacji: {1}", state.ToString(), j.ToString()));
+                    }
+                    // wysłamy zdarzenie za pomocą metody Set(), zmiana wartości sygnału, który pozwala by jeden lub więcej wątków oczekiwał ma wykonanie zdarzenia
+                    mres[id].Set();
+                });
+                // Uruchamiany wykonanie danego wątku
+                thread.Start(string.Format("Wątek: {0}", i));
+            }
+            // WaitHandle - mechanim pozwalający na kontrolę nad wykonaniem wątków
+            // Oczekujemy, aż każdy element na liście otrzyma sygnał
+            WaitHandle.WaitAll((from x in mres select x.WaitHandle).ToArray());
+            // Zapisujemy czas wykonania klasycznego wątku
+            var threadTime = watch.ElapsedMilliseconds;
+            watch.Reset();
+
+
+
+
+
+
+            foreach (ManualResetEventSlim t in mres)
+            {
+                // zmieniamy status zdarzenia na brak sygnału, powoduje to zablokowanie wątków
+                t.Reset();
+            }
+
+
+
+
+
+
+
+
+            watch.Start();
+            // Wykonujemy to samo zadanie, tym razem używając biblioteki Task Parallel Library
+            for (int i = 0; i < mres.Length; i++)
+            {
+                var id = i;
+                Task.Factory.StartNew(state =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.WriteLine(string.Format("Wątek: {0}, numer operacji: {1}", state.ToString(), j.ToString()));
+                    }
+                    mres[id].Set();
+                }, string.Format("Zadanie: {0}", i));
+            }
+            WaitHandle.WaitAll((from x in mres select x.WaitHandle).ToArray());
+            // zapisujemy czas wykonania zadania
+            var taskTime = watch.ElapsedMilliseconds;
+
+
+
+
+            // wypisujemy czasy wykonania operacji
+            Console.WriteLine("Tradycyjne podejście przy użyciu wątków: {0}ms", threadTime);
+            Console.WriteLine("Podejście przy użyciu TPL: {0}ms", taskTime);
+            foreach (var item in mres)
+            {
+                // zmieniamy status zdarzenia na brak sygnału, powoduje to zablokowanie wątków
+                item.Reset();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadAndTask/test1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+namespace ParallelExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ustawiamy rozmiary naszych tablic.
+            // Zwiększ liczbę elementów aby lepiej zobaczyć porównanie uzyskanych rezultatów
+            int colCounter = 800;
+            int rowCounter = 800;
+            // tworzymy dwuwymiarowe tablice
+            double[,] m1 = InitializeMatrix(rowCounter, colCounter);
+            double[,] m2 = InitializeMatrix(rowCounter, colCounter);
+            // tworzymy tablicę wynikową. Będzie ona rezultatem mnożenia obu tablic
+            var result = new double[rowCounter, colCounter];
+            // W pierwszej kolejności wykonamy tradycyjne podejście
+            Console.WriteLine("Tradycyjne wykonanie... \nOperacja ta chwilę potrwa...");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            // mnożenie naszych tablic
+            MultiplyTables(m1, m2, result);
+            stopwatch.Stop();
+            Console.WriteLine("Czas operacji: {0}ms", stopwatch.ElapsedMilliseconds);
+            // resetujemy stoper oraz tworzymy nową tablicę wyników
+            stopwatch.Reset();
+            Console.WriteLine("Podejście równoległe...");
+            result = new double[rowCounter, colCounter];
+            stopwatch.Start();
+            MultiplyTablesParallel(m1, m2, result);
+            Console.WriteLine("Czas operacji: {0}ms", stopwatch.ElapsedMilliseconds);
+            stopwatch.Stop();
+            Console.ReadKey();
+            // Wynik działania programu dla 800 kolumn i wierszy
+            // Tradycyjne wykonanie...
+            // Operacja ta chwile potrwa...
+            // Czas operacji: 11136ms
+            // Podejscie równolegle...
+            // Czas operacji: 5046ms
+        }
+        private static void MultiplyTablesParallel(double[,] m1, double[,] m2, double[,] result)
+        {
+            // zwracamy liczbe kolumn we wskazanym wymiarze
+            int m1Columns = m1.GetLength(1);
+            int m2Columns = m2.GetLength(1);
+            int resultColumns = result.GetLength(0);
+            // Wykonany dokładnie tą samą operację ale przy użyciu biblioteki TPL
+            Parallel.For(0, m1Columns, i =>
+            {
+                for (int j = 0; j < m2Columns; j++)
+                {
+                    // zmienna tymczasowa pozwoli na optymalizację równległych obliczeń
+                    double temp = 0;
+                    for (int k = 0; k < m1Columns; k++)
+                    {
+                        temp += m1[i, k] * m2[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            });
+        }
+        private static void MultiplyTables(double[,] m1, double[,] m2, double[,] result)
+        {
+            // zwracamy liczbe kolumn we wskazanym wymiarze
+            int m1Columns = m1.GetLength(1);
+            int m2Columns = m2.GetLength(1);
+            int resultColumns = result.GetLength(0);
+            for (int i = 0; i < m1Columns; i++)
+            {
+                for (int j = 0; j < m2Columns; j++)
+                {
+                    // zmienna tymczasowa pozwoli na optymalizację obliczeń
+                    double temp = 0;
+                    for (int k = 0; k < m1Columns; k++)
+                    {
+                        temp += m1[i, k] * m2[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }
+        }
+        private static double[,] InitializeMatrix(int rowCounter, int colCounter)
+        {
+            var table = new double[rowCounter, colCounter];
+            // klasa dzięki, której możemy generować liczby losowe
+            var r = new Random();
+            for (int i = 0; i < rowCounter; i++)
+            {
+                for (int j = 0; j < colCounter; j++)
+                {
+                    // liczba losowa, gdzie maksymalna wartość jest mniejsza niż 100
+                    table[i, j] = r.Next(100);
+                }
+            }
+            return table;
+        }
+    }
+}
