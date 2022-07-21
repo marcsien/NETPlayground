cea262770e45fe9e180a86fbf69ff4ca49d80e54
marcsien <marcsien@gmail.com>
2022-07-22 00:41:57 +0200

    Add ShapeFactory implementation project and delete bin and obj folders in old projects.

---
+++ DesignPatterns/ShapeFactoryExample/ShapeFactoryExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ShapeFactoryExample;
+
+Console.WriteLine("Hello, ShapeFactory !");
+
+ShapeFactory shapeFactory = new ShapeFactory();
+
+IShape shape1 = shapeFactory.GetShape("CIRCLE");
+shape1.Draw();
+
+IShape shape2 = shapeFactory.GetShape("RECTANGLE");
+shape2.Draw();
+
+IShape shape3 = shapeFactory.GetShape("SQUARE");
+shape3.Draw();
---
+++ DesignPatterns/ShapeFactoryExample/ShapeFactoryExample/ShapeFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeFactoryExample
+{
+    internal class ShapeFactory
+    {
+        public IShape GetShape(string shapeType)
+        {
+            if (shapeType == null)
+            {
+                return null;
+            }
+
+            if (shapeType.Equals("CIRCLE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Circle();
+            }
+            else if (shapeType.Equals("SQUARE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Square();
+            }
+            else if (shapeType.Equals("RECTANGLE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Rectangle();
+            }
+
+            return null;
+        }
+    }
+}
