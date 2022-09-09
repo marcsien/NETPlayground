3c09ed2cf4cb9c556cdde7e20a7fb21f7d9e337e
marcsien <marcsien@gmail.com>
2022-09-09 07:23:21 +0200

    Singleton Pattern demo project.

---
+++ DesignPatterns/SingletonExample/SingletonExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using SingletonExample;
+
+Console.WriteLine("Singleton demo!");
+
+//illegal construct
+//Compile Time Error: The constructor SingleObject() is not visible
+//SingleObject object = new SingleObject();
+
+//Get the only object available
+SingleObject obj = SingleObject.GetInstance();
+
+//show the message
+obj.ShowMessage();
