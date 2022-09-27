6d08a374d641998d5b8a16011013704f8c62b5b1
marcsien <marcsien@gmail.com>
2022-09-27 07:46:17 +0200

    Bridge design pattern example

---
+++ DesignPatterns/ShapeBridgeExample/ShapeBridgeExample/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ShapeBridgeExample;
+
+Console.WriteLine("Hello, Bridge Pattern !");
+
+
+Shape redCircle = new Circle(100, 100, 10, new RedCircle());
+Shape greenCircle = new Circle(100, 100, 10, new GreenCircle());
+
+redCircle.draw();
+greenCircle.draw();
