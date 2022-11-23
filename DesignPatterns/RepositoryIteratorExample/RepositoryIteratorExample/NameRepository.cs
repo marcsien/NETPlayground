4924b09be8293f6a3662051c20e207f8bd1da94d
marcsien <marcsien@gmail.com>
2022-11-23 07:16:23 +0100

    Iterator Design pattern demo

---
+++ DesignPatterns/RepositoryIteratorExample/RepositoryIteratorExample/NameRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryIteratorExample
+{
+    public class NameRepository : IContainer
+    {
+        public static string[] names = { "Marcin", "Kasia", "Daniel", "Łukasz" };
+        public IIterator getIterator()
+        {
+            return new NameIterator();
+        }
+
+        private class NameIterator : IIterator
+        {
+            int index;
+            public bool hasNext()
+            {
+                if (index < names.Length)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public object next()
+            {
+                if (this.hasNext())
+                {
+                    return names[index++];
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ DesignPatterns/RepositoryIteratorExample/RepositoryIteratorExample/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RepositoryIteratorExample;
+
+Console.WriteLine("Hello, Iterator Pattern!");
+
+
+NameRepository namesRepository = new NameRepository();
+
+for (IIterator iter = namesRepository.getIterator(); iter.hasNext();)
+{
+    string name = (string)iter.next();
+    Console.WriteLine("Name : " + name);
+}
